f640a05b371797d3e027ebfc4c8eaef89e71c8ad
Jeevajithin <jeeva.sics@gmail.com>
2024-05-09 18:04:31 +0530

    index file

---
+++ index.php
@@ -0,0 +1,189 @@
+<?php
+include('phpfiles/header.php');
+?>
+            <!-- Nav Bar End -->
+            
+            
+            <!-- Carousel Start -->
+            <div id="carousel" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel" data-slide-to="1"></li>
+                    <li data-target="#carousel" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="template/img/slide1.jpg" alt="Carousel Image">
+                        <div class="carousel-caption">
+                            <h1 class="animated fadeInLeft">We fight for your justice</h1>
+                            <p class="animated fadeInRight">Empowering Legal Excellence, One Click at a Time.</p>
+                            <a class="btn animated fadeInUp" href="phpfiles/login.php">Join with Us</a>
+                        </div>
+                    </div>
+
+                    <div class="carousel-item">
+                        <img src="template/img/carousel-2.jpg" alt="Carousel Image">
+                        <div class="carousel-caption">
+                            <h1 class="animated fadeInLeft">We prepared to oppose for you</h1>
+                            <p class="animated fadeInRight">Efficiency Redefined: Your Legal Companion.</p>
+                            <a class="btn animated fadeInUp" href="phpfiles/login.php">Join with Us</a>
+                        </div>
+                    </div>
+
+                    <div class="carousel-item">
+                        <img src="template/img/carousel-3.jpg" alt="Carousel Image">
+                        <div class="carousel-caption">
+                            <h1 class="animated fadeInLeft">We fight for your privilege</h1>
+                            <p class="animated fadeInRight">Seamless Collaboration, Superior Results</p>
+                            <a class="btn animated fadeInUp" href="phpfiles/login.php">Join with Us</a>
+                        </div>
+                    </div>
+                </div>
+
+                <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+            <!-- Carousel End -->
+            
+            
+            <!-- Top Feature Start-->
+            <div class="feature-top">
+                <div class="container-fluid">
+                    <div class="row align-items-center">
+                        <div class="col-md-3 col-sm-6">
+                            <div class="feature-item">
+                                <i class="far fa-check-circle"></i>
+                                <h3>Legal</h3>
+                                <p>Govt Approved</p>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="feature-item">
+                                <i class="fa fa-user-tie"></i>
+                                <h3>Attorneys</h3>
+                                <p>Expert Attorneys</p>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="feature-item">
+                                <i class="far fa-thumbs-up"></i>
+                                <h3>Success</h3>
+                                <p>99.99% Case Won</p>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="feature-item">
+                                <i class="far fa-handshake"></i>
+                                <h3>Support</h3>
+                                <p>Quick Support</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Top Feature End-->
+            
+            
+            <!-- About Start -->
+            <div class="about">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-5 col-md-6">
+                            <div class="about-img">
+                                <img src="template/img/about.jpg" alt="Image">
+                            </div>
+                        </div>
+                        <div class="col-lg-7 col-md-6">
+                            <div class="section-header">
+                                <h2>Learn About Us</h2>
+                            </div>
+                            <div class="about-text">
+                               <p>Welcome to our platform, where innovation meets efficiency in the legal landscape. We pride ourselves on offering a cutting-edge solution designed to simplify and streamline the complexities of legal processes. Our platform serves as a hub for administrators, advocates, judges, and court coordinators alike, providing intuitive tools and resources tailored to their respective roles.</p>
+							
+							<p>For administrators, our feature-rich dashboard empowers seamless management of user accounts, including the addition and removal of judges, verification of advocate credentials, and appointment of court coordinators. </p>
+                                <a class="btn" href="phpfiles/about_us.php">Learn More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- About End -->
+
+
+            <!-- Service Start -->
+            
+            <!-- Service End -->
+            
+            
+            <!-- Feature Start -->
+            <div class="feature">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-7">
+                            <div class="section-header">
+                                <h2>Why Choose Us</h2>
+                            </div>
+                            <div class="row align-items-center feature-item">
+                                <div class="col-5">
+                                    <div class="feature-icon">
+                                        <i class="fa fa-gavel"></i>
+                                    </div>
+                                </div>
+                                <div class="col-7">
+                                    <h3>Best law practices</h3>
+                                    <p>
+                                    Implementing superior methods and strategies to provide exceptional legal services and uphold the highest standards of professionalism and client satisfaction.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="row align-items-center feature-item">
+                                <div class="col-5">
+                                    <div class="feature-icon">
+                                        <i class="fa fa-balance-scale"></i>
+                                    </div>
+                                </div>
+                                <div class="col-7">
+                                    <h3>Efficiency & Trust</h3>
+                                    <p>
+                                    Delivering services with a focus on both effectiveness and reliability, ensuring that you can rely on us to achieve your goals efficiently and with unwavering trust.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="row align-items-center feature-item">
+                                <div class="col-5">
+                                    <div class="feature-icon">
+                                        <i class="far fa-smile"></i>
+                                    </div>
+                                </div>
+                                <div class="col-7">
+                                    <h3>Results you deserve</h3>
+                                    <p>
+                                    Ensuring that you receive the outcomes you rightfully merit through our dedicated efforts and commitment to excellence in every aspect of our service.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-5">
+                            <div class="feature-img">
+                                <img src="template/img/feature.jpg" alt="Feature">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Feature End -->
+            
+            
+
+          
+            
+
+
+            <!-- Footer Start -->
+            <?php include('phpfiles/footer.php'); ?>
